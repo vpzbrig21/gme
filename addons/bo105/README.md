9a413a3366e8b256f4435c40498b4a6092acb4ad
sancron <danielsoro.de@gmail.com>
2020-08-19 18:29:14 +0200

    Bo105 Base

---
+++ addons/bo105/README.md
@@ -0,0 +1,11 @@
+gme_bo105
+===================
+
+Adds blank feature.
+
+
+## Maintainers
+
+The people responsible for merging changes to this component or answering potential questions.
+
+- [Name](https://github.com/name)
---
+++ addons/bo105/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"gme_main","gm_vehicles_air_helicopters_bo105"};
+        author = ECSTRING(main,GMETeam);
+        authors[] = {""};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.inc"
