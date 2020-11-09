44c207f9f4857cb4622aad4e41c86ae9f66e70ce
sancron <danielsoro.de@gmail.com>
2020-11-09 04:09:46 +0100

    Belt and Grenade Fix
    Adding belt-funcitonality to MG3 and PKM
    Removed fragmentation from Conc and Practice greandes

---
+++ addons/ammo/README.md
@@ -0,0 +1,11 @@
+gme_ammo
+===================
+
+Adds ACE3 features to Ammo and Magazines.
+
+
+## Maintainers
+
+The people responsible for merging changes to this component or answering potential questions.
+
+- [sancron](https://github.com/sancron)
---
+++ addons/ammo/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"gme_main"};
+        author = ECSTRING(main,GMETeam);
+        authors[] = {""};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgAmmo.inc"
+#include "CfgMagazines.inc"
