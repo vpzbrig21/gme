35a3918ad731d1f27cce65bf647439abbde45b69
sancron <danielsoro.de@gmail.com>
2020-11-10 21:27:30 +0100

    Adding Ural375d base closes #10

---
+++ addons/ural375d/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT ural375d
+#define COMPONENT_BEAUTIFIED Ural375d
+#include "\z\gme\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_BLANK
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BLANK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
+#endif
+
+#include "\z\gme\addons\main\script_macros.hpp"
