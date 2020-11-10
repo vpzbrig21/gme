f6847a021ade16e9c0352deedf6e2943d0ae3df8
sancron <danielsoro.de@gmail.com>
2020-11-10 19:02:27 +0100

    Adding Mi2 base close #4

---
+++ addons/mi2/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"gme_main","gm_vehicles_air_helicopters_mi2"};
+        author = ECSTRING(main,GMETeam);
+        authors[] = {""};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.inc"
