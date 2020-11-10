5de97c7f31ff0bce06bd58651950295954c6bf30
sancron <danielsoro.de@gmail.com>
2020-11-10 15:55:49 +0100

    Adding base for BPz2 #14

---
+++ addons/bpz2/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"gme_main","gm_vehicles_land_tracked_leopard1","gm_vehicles_land_tracked_bpz2"};
+        author = ECSTRING(main,GMETeam);
+        authors[] = {""};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.inc"
