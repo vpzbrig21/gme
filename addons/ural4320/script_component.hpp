d060b10c0cd58530a65017ccf4d9fc11803c6865
sancron <danielsoro.de@gmail.com>
2020-11-10 22:34:23 +0100

    Adding Ural 4320 base closes #11

---
+++ addons/ural4320/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT ural4320
+#define COMPONENT_BEAUTIFIED Ural4320
+#include "\z\gme\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_BLANK
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BLANK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
+#endif
+
+#include "\z\gme\addons\main\script_macros.hpp"
