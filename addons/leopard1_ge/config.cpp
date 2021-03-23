c54461f75aa373b1b0a5723955ac7729d445eef3
sancron <danielsoro.de@gmail.com>
2021-03-23 09:50:31 +0100

    Leopard 1 basic radio config Adding SEM80/90 to corresponding vehicles #18

--- addons/leopard1_ge/config.cpp
+++ addons/leopard1_ge/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"gme_main","gme_leopard1"};
+        requiredAddons[] = {"gme_main","gme_leopard1","gm_vehicles_land_tracked_leopard1_ge_army_leopard1"};
         author = ECSTRING(main,GMETeam);
         authors[] = {""};
         url = ECSTRING(main,URL);
