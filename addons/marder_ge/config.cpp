fce68c871ccd04faa51b28c611eb319cf9261e92
sancron <danielsoro.de@gmail.com>
2021-03-24 15:37:04 +0100

    Adding Marder Update 1.3 vehicle compatibility #20

---
+++ addons/marder_ge/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"gme_main","gme_marder","gm_vehicles_land_tracked_marder_ge_army_marder1"};
+        author = ECSTRING(main,GMETeam);
+        authors[] = {""};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.inc"
