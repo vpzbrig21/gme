b0fd2daf8d9334932f8e75485b23a29544109e44
sancron <danielsoro.de@gmail.com>
2021-05-11 13:04:10 +0200

    Special PB21 Marder

---
+++ optionals/marder_pb21/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT marder1_pb21
+#define COMPONENT_BEAUTIFIED Marder1_PB21
+#include "\z\gme\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_BLANK
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BLANK
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
+#endif
+
+#include "\z\gme\addons\main\script_macros.hpp"
