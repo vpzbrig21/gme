61f75fa55e53fe27760507c82ef705814db0750b
sancron <danielsoro.de@gmail.com>
2021-09-08 17:33:09 +0200

    Fix missing Name

--- addons/backpacks/script_component.hpp
+++ addons/backpacks/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT blank
-#define COMPONENT_BEAUTIFIED Blank
+#define COMPONENT backpacks
+#define COMPONENT_BEAUTIFIED Backpacks
 #include "\z\gme\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
