965b6346e0c31b1ebed86a51d83083a1bed41c3c
sancron <danielsoro.de@gmail.com>
2023-10-19 13:38:02 +0200

    Adding more Crests

--- addons/main/CfgVehicleInsignias.hpp
+++ addons/main/CfgVehicleInsignias.hpp
@@ -8,6 +8,7 @@ class gm_VehicleInsignias_Nations: gm_VehicleInsignias_base {
 		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\nation\gme_at_army_01-2_ca.paa";
 	};
 };
+
 class gm_VehicleInsignias_Unit: gm_VehicleInsignias_base {
 	class gme_units_ge_pzbrig21_col {
 		name = "PzBrig 21";
@@ -58,14 +59,110 @@ class gm_VehicleInsignias_Unit: gm_VehicleInsignias_base {
 		value = "gme_units_ge_pzbtl363_col";
 		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_pzbtl363_col_ca.paa";
 	};
-    class gme_units_ge_pzgrenbtl401_col {
+	class gme_units_ge_aufklbtl7_col {
+		name = "AufklBtl 7";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_aufklbtl7_col_ca.paa";
+		value = "gme_units_ge_aufklbtl7_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_aufklbtl7_col_ca.paa";
+	};
+	class gme_units_ge_versbtl7_col {
+		name = "AufklBtl 7";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_versbtl7_col_ca.paa";
+		value = "gme_units_ge_versbtl7_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_versbtl7_col_ca.paa";
+	};
+	class gme_units_ge_gebjgbtl233_col {
+		name = "GebJgBtl 233";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_gebjgbtl233_col_ca.paa";
+		value = "gme_units_ge_gebjgbtl233_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_gebjgbtl233_col_ca.paa";
+	};
+	class gme_units_ge_pzlehrbtl93_col {
+		name = "PzLehrBtl 93";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_pzlehrbtl93_col_ca.paa";
+		value = "gme_units_ge_pzlehrbtl93_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_pzlehrbtl93_col_ca.paa";
+	};
+
+	/* Arma Units */
+	class gme_units_a3_vpzbrig21_col {
+		name = "vPzBrig 21";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_vpzbrig21_col_ca.paa";
+		value = "gme_units_a3_vpzbrig21_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_vpzbrig21_col_ca.paa";
+	};
+	class gme_units_a3_pzgrenbtl401_col {
 		name = "PzGrenBtl 401";
 		author = "ingametek";
 		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_pzgrenbtl401_col_ca.paa";
-		value = "gme_units_ge_pzgrenbtl401_col";
+		value = "gme_units_a3_pzgrenbtl401_col";
 		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_pzgrenbtl401_col_ca.paa";
 	};
+	class gme_units_a3_fec_col {
+		name = "First Euro Corps";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_fec_col_ca.paa";
+		value = "gme_units_a3_fec_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_ge_fec_col_ca.paa";
+	};
+
+	/* Girls und Panzer Fun */
+	class gme_units_gup_ooarai_col {
+		name = "Ōarai";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_ooarai_col_ca.paa";
+		value = "gme_units_gup_ooarai_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_ooarai_col_ca.paa";
+	};
+	class gme_units_gup_ooarai_col {
+		name = "Schwarzwaldgipfel";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_kuromorimine_col_ca.paa";
+		value = "gme_units_gup_kuromorimine_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_kuromorimine_col_ca.paa";
+	};
+	class gme_units_gup_stgloriana_col {
+		name = "St. Gloriana";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_stgloriana_col_ca.paa";
+		value = "gme_units_gup_stgloriana_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_stgloriana_col_ca.paa";
+	};
+	class gme_units_gup_saunders_col {
+		name = "Saunders";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_saunders_col_ca.paa";
+		value = "gme_units_gup_saunders_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_saunders_col_ca.paa";
+	};
+	class gme_units_gup_pravda_col {
+		name = "Prawda";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_pravda_col_ca.paa";
+		value = "gme_units_gup_pravda_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_pravda_col_ca.paa";
+	};
+	class gme_units_gup_anzio_col {
+		name = "Anzio";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_anzio_col_ca.paa";
+		value = "gme_units_gup_anzio_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_anzio_col_ca.paa";
+	};
+	class gme_units_gup_bcfreedom_col {
+		name = "BC Freedom";
+		author = "ingametek";
+		texture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_bcfreedom_col_ca.paa";
+		value = "gme_units_gup_bcfreedom_col";
+		picture = "z\gme\addons\main\data\systems\vehicleMarkings\insignias\crest\gme_gup_bcfreedom_col_ca.paa";
+	};
 };
+
 class gm_VehicleInsignias_Company: gm_VehicleInsignias_base {
 	class gme_insignia_cool_363_wht {
 		name = "363 Büffel (Weiß)";
